a8bd10b83e97e81f6fb741257ff28f77c9d149ca
John Leonard <thejohnleonard@gmail.com>
2016-07-30 13:41:25 -0400

    adding icon to quote page

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -6,7 +6,7 @@ var jazzicon = require('jazzicon');
     var icons = document.querySelectorAll('.user-icon');
     Array.prototype.forEach.call(icons, function(el, i){
       var num = parseInt( el.getAttribute('data-icon'), 10);
-      var icon = jazzicon(32, num);
+      var icon = jazzicon(48, num);
       el.appendChild(icon);
     });
   }); 
